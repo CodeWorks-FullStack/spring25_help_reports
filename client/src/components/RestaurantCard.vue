fc0ec27a5aa29294a43d1ad2add6eb6a6a0e592c
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-23 12:42:07 -0600

    added owner info to card

--- client/src/components/RestaurantCard.vue
+++ client/src/components/RestaurantCard.vue
@@ -10,11 +10,13 @@ defineProps({
 <template>
   <RouterLink :to="{ name: 'Restaurant Details', params: { restaurantId: restaurant.id } }"
     :title="`Go to the ${restaurant.name} page`">
-    <div class="shadow-lg">
-      <img :src="restaurant.imgUrl" :alt="`A picture of the ${restaurant.name} restaurant`"
-        class="w-100 restaurant-img">
+    <div class="shadow-lg mb-3 position-relative">
+      <img :src="restaurant.imgUrl" :alt="`A picture of the ${restaurant.name} restaurant`" class="w-100 restaurant-img"
+        :class="{ 'gray-out': restaurant.isShutdown }">
       <div class="bg-light p-2">
-        <span class="text-green fw-bold">{{ restaurant.name }}</span>
+        <span class="fw-bold" :class="restaurant.isShutdown ? 'text-danger' : 'text-green'">
+          {{ restaurant.name }}
+        </span>
         <p>{{ restaurant.description }}</p>
         <div class="d-flex justify-content-between">
           <div>
@@ -27,6 +29,10 @@ defineProps({
           </div>
         </div>
       </div>
+      <div>
+        <img :src="restaurant.owner.picture" :alt="restaurant.owner.name" class="owner-img m-1"
+          :title="'The owner is ' + restaurant.owner.name">
+      </div>
     </div>
   </RouterLink>
 </template>
@@ -36,5 +42,19 @@ defineProps({
 .restaurant-img {
   height: 40dvh;
   object-fit: cover;
+}
+
+.gray-out {
+  filter: grayscale(1);
+}
+
+.owner-img {
+  height: 4em;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
