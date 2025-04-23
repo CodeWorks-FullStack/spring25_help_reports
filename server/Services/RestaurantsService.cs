dfee2cc93ca894cd3270213eb1a809374172856a
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-23 09:38:48 -0600

    restaurant files

---
+++ server/Services/RestaurantsService.cs
@@ -0,0 +1,11 @@
+namespace help.Services;
+
+public class RestaurantsService
+{
+  public RestaurantsService(RestaurantsRepository repository)
+  {
+    _repository = repository;
+  }
+  private readonly RestaurantsRepository _repository;
+
+}
